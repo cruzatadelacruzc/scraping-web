d47e8b6541082fe1150869bf9022527049abb1e7
Cesar Manuel Cruzata De la Cruz <cruzatadelacruzc@gmail.com>
2024-09-03 14:58:57 -0400

    refactor: 🔥 remove deprecated file
    Delete mock test model

--- src/__tests__/models/mock-model.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const mockSchema = new Schema({
-  name: { type: String, required: true },
-});
-
-const MockModel = model('Mock', mockSchema);
-
-export default MockModel;
