492caa088556096e44989b5d0adab97d2d11339c
Cesar Manuel Cruzata De la Cruz <cruzatadelacruzc@gmail.com>
2024-09-12 16:26:25 -0400

    perf(DB): ⚡️ update db configuration

--- src/config/db-config.ts
+++ src/config/db-config.ts
@@ -1,19 +1,30 @@
-import { connect } from 'mongoose';
+import { injectable } from 'inversify';
+import mongoose from 'mongoose';
 
-/**
- * Create connection with MongoDB database.
- *
- * @param uri - The URI of the MongoDB database.
- */
-async function dbConnect(): Promise<void> {
-  const DB_URI = <string>process.env.DB_URI;
-  try {
-    await connect(DB_URI);
-    console.log('Conexión a la base de datos establecida.');
-  } catch (error) {
-    console.error('🔥 ', error);
-    throw error;
+@injectable()
+export class DBContext {
+  private _db: typeof mongoose;
+
+  /**
+   * Create connection with MongoDB database.
+   *
+   * @param uri - The URI of the MongoDB database.
+   */
+  async dbConnect(): Promise<void> {
+    const DB_URI = <string>process.env.DB_URI;
+    try {
+      this._db = await mongoose.connect(DB_URI);
+      console.log('Database connection established.');
+    } catch (error) {
+      console.error('🔥 ', error);
+      throw error;
+    }
+  }
+
+  get db(): typeof mongoose {
+    if (!this._db) {
+      throw new Error('The database connection has not been initialized.');
+    }
+    return this._db;
   }
 }
-
-export default dbConnect;
