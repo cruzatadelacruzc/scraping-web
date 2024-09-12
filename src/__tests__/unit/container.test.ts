08f4b60abd8cdcc740579596b8c1cf0787ef7578
Cesar Manuel Cruzata De la Cruz <cruzatadelacruzc@gmail.com>
2024-09-12 16:25:18 -0400

    test(DB): ✅ add dependency injection

---
+++ src/__tests__/unit/container.test.ts
@@ -0,0 +1,19 @@
+import { TYPES } from '@shared/types.container';
+import { container } from '@shared/container';
+import { IFetchProductData } from '@shared/fetch-product-data.interfaces';
+
+
+describe('Inversify Container', () => {
+    it('should resolve the service correctly from the container', async () => {
+      let service: IFetchProductData;
+
+      service = container.get<IFetchProductData>(TYPES.RevolicoData);
+
+      // Verify service definition
+      expect(service).toBeDefined();
+      // Verify that the attribute exists
+      expect(service).toHaveProperty('_baseURL');
+      // Verify that the method exists
+      expect(service.fetchProductInfoByCategory).toBeInstanceOf(Function);
+    });
+})
