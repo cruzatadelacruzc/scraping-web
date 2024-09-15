548f8b5e1a868b324898c8ab20cf2aac9c148190
Cesar Manuel Cruzata De la Cruz <cruzatadelacruzc@gmail.com>
2024-09-15 14:27:38 -0400

    perf(Container): ⚡️ update default scope configuration
    default scope singleton to transient

--- src/shared/container.ts
+++ src/shared/container.ts
@@ -3,20 +3,24 @@ import {RevolicoController} from '@scrapers/revolico/controllers/revolico.contro
 import { Logger } from '@shared/logger';
 import { RevolicoFetchDataService } from '@scrapers/revolico/services/fetch-data.service';
 import { DBContext } from '@config/db-config';
+
 import { IFetchProductData } from '@shared/fetch-product-data.interfaces';
 import { TYPES } from '@shared/types.container';
 import { ILogger } from '@shared/logger.interfaces';
 
-export const container = new Container({
-  defaultScope: 'Singleton'
-});
+
+
+export const container = new Container();
 
 //shared
-container.bind<ILogger>(TYPES.Logger).to(Logger);
-container.bind(DBContext).toSelf();
+container.bind<ILogger>(TYPES.Logger).to(Logger).inSingletonScope();
+container.bind(DBContext).toSelf().inSingletonScope();
+
 
 //services
 container.bind<IFetchProductData>(TYPES.RevolicoData).to(RevolicoFetchDataService);
+
+
 
 //controllers
 container.bind(RevolicoController).toSelf();
