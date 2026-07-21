598ca017f71a3f350339ad9fec11b5e2fd301e43
Cesar Manuel Cruzata De la Cruz <cruzatadelacruzc@gmail.com>
2026-07-21 10:26:32 -0400

    chore: Clean up icon files and dev artifacts

--- apps/customer-dashboard/dev-dist/sw.js
+++ apps/customer-dashboard/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-d488705a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3dmo9427ulo"
+    "revision": "0.fpl1su0mkco"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
